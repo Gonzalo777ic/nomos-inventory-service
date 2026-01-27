6f17da9a90d7a03acdc0ebedc71bd04ad5a429ab
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-26 23:10:57 -0500

    importaciones de paquetes necesarios para el modelo alert

--- src/main/java/com/nomos/inventory/service/model/Alert.java
+++ src/main/java/com/nomos/inventory/service/model/Alert.java
@@ -1,4 +1,9 @@
 package com.nomos.inventory.service.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class Alert {
 }
