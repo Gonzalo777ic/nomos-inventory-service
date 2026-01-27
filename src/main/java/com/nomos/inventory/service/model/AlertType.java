7e8004d5fe523054830e7956c3b5f1a26203ac28
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-26 23:08:48 -0500

    definición de constantes y de variable privada displayvalue

--- src/main/java/com/nomos/inventory/service/model/AlertType.java
+++ src/main/java/com/nomos/inventory/service/model/AlertType.java
@@ -2,6 +2,13 @@ package com.nomos.inventory.service.model;
 
 public enum AlertType {
 
+    LOW_STOCK("Stock Bajo"),
+    CRITICAL_STOCK("Stock Crítico"),
+    NEAR_EXPIRATION("Próximo a Vencer"),
+    EXPIRED("Vencido");
+    private final String displayValue;
 
-
+    AlertType(String displayValue) {
+        this.displayValue = displayValue;
+    }
 }
