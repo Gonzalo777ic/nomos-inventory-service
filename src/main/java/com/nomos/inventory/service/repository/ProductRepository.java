9bf4d1d42486f387d0d70df33705ae68f5120b46
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-26 23:24:21 -0500

    modificación de repository de product

--- src/main/java/com/nomos/inventory/service/repository/ProductRepository.java
+++ src/main/java/com/nomos/inventory/service/repository/ProductRepository.java
@@ -17,5 +17,19 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsBySku(String sku);
 
     boolean existsBySkuAndIdNot(String sku, Long id);
-
+    @Query("SELECT new com.nomos.inventory.service.model.dto.StockAlertDTO(" +
+            "p.id, " +
+            "p.name, " +
+            "p.sku, " +
+            "(SELECT MAX(pi.imageUrl) FROM ProductImage pi WHERE pi.product.id = p.id AND pi.isMain = true), " + // <--- CAMBIO AQUÍ
+            "CAST(COALESCE(SUM(i.quantity), 0) AS int), " +
+            "p.minStockThreshold, " +
+            "CAST((p.minStockThreshold - COALESCE(SUM(i.quantity), 0)) AS int), " +
+            "'LOW') " +
+            "FROM Product p " +
+            "LEFT JOIN InventoryItem i ON i.product.id = p.id " +
+            "WHERE p.minStockThreshold IS NOT NULL " +
+            "GROUP BY p.id, p.name, p.sku, p.minStockThreshold " + // Quitamos p.imageUrl del group by
+            "HAVING COALESCE(SUM(i.quantity), 0) <= p.minStockThreshold")
+    List<StockAlertDTO> findProductsWithLowStock();
 }
