0d2f36e11c231688b949f024a11f024bbc11bd2f
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-26 23:14:57 -0500

    importación de los paquetes necesarios

--- src/main/java/com/nomos/inventory/service/repository/AlertRepository.java
+++ src/main/java/com/nomos/inventory/service/repository/AlertRepository.java
@@ -1,4 +1,15 @@
 package com.nomos.inventory.service.repository;
 
-public class AlertRepository {
+import com.nomos.inventory.service.model.Alert;
+import com.nomos.inventory.service.model.AlertStatus;
+import com.nomos.inventory.service.model.AlertType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+
+
 }
